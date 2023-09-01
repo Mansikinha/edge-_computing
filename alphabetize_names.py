2e5a9413a165e13dc2a2d457a80d86778c54038d
choudharymansi279 <choudharymansi279@gmail.com>
2023-09-01 22:34:46 +0530

    sorting of alphabets

---
+++ alphabetize_names.py
@@ -0,0 +1,9 @@
+# List of full names
+names = [" code", "Python", "java", "Edge computing"]
+
+# Sort the names alphabetically
+sorted_names = sorted(names)
+
+# Print the sorted names
+for name in sorted_names:
+    print(name)
