051946c5b05002baca8538364b7ba70b79a4cfd9
choudharymansi279 <choudharymansi279@gmail.com>
2023-09-10 23:38:59 +0530

    counts words

---
+++ word_count.py
@@ -0,0 +1,23 @@
+def wordcount(filename):
+    counts = {'characters': 0,
+              'words': 0,
+              'lines': 0}
+    unique_words = set()
+
+    try:
+        with open(filename, 'r') as file:
+            for one_line in file:
+                counts['lines'] += 1
+                counts['characters'] += len(one_line)
+                counts['words'] += len(one_line.split())
+                unique_words.update(one_line.split())
+
+        counts['unique words'] = len(unique_words)
+
+        for key, value in counts.items():
+            print(f'{key}: {value}')
+
+    except FileNotFoundError:
+        print(f"Error: {filename} not found")
+
+wordcount('wcfile.txt')
