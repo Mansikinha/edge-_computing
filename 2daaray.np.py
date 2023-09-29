dd47fa0274ff8e832fb1c335d26cb6febc8fdee0
choudharymansi279 <choudharymansi279@gmail.com>
2023-09-29 21:23:16 +0530

    create 2D array

---
+++ 2daaray.np.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Create a 1D array
+arr_1d = np.array([1, 2, 3, 4, 5])
+print("1D Array:", arr_1d)
+
+# Create a 2D array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print("\n2D Array:\n", arr_2d)
