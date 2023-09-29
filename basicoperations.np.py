69db862eb0dc9f905ff155453336a82f2647aa51
choudharymansi279 <choudharymansi279@gmail.com>
2023-09-29 21:35:18 +0530

    basic operations add,sub , div

---
+++ basicoperations.np.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.array([1 , 2, 3])
+arr2 =  np.array ([ 4 , 5 , 6])
+  
+result_add = arr1 + arr2
+result_sub = arr2 - arr1
+result_div = arr2 / arr1
+
+print("addition:", result_add)
+print ("subtraction:", result_sub)
+print("division:", result_div)
