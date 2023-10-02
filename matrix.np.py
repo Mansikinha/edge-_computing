9c9ce68f5348b7e6b7591d8bb372fe52d553a2a2
choudharymansi279 <choudharymansi279@gmail.com>
2023-10-02 19:27:02 +0530

    matrix

---
+++ matrix.np.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Matrix multiplication
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+
+result_matrix = np.dot(matrix1, matrix2)
+print("Matrix Multiplication:\n", result_matrix)
