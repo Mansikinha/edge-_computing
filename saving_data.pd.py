9f5c787b3164dd26c86ef4205d486b289e7a4a30
choudharymansi279 <choudharymansi279@gmail.com>
2023-10-11 23:22:04 +0530

    saving data

--- saving_data.pd.py
+++ saving_data.pd.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-
+#creating data
 data = {'Column1': [1, 2, 3], 'Column2': ['A', 'B', 'C']}
 df = pd.DataFrame(data)
 
+#printing data frame
 print("DataFrame:")
 print(df)
 
+#saving data frame to csv
 df.to_csv('output.csv', index=False)
 
 print("CSV file 'output.csv' has been created.")
+
