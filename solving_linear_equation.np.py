15d36418175899b971c2fb76cbd03b2d7835a806
choudharymansi279 <choudharymansi279@gmail.com>
2023-10-16 22:05:17 +0530

    linear equation numpy

---
+++ solving_linear_equation.np.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Coefficient matrix
+A = np.array([[2, 1], [1, -3]])
+
+# Right-hand side vector
+B = np.array([8, 1])
+
+# Solve linear equations Ax = B
+x = np.linalg.solve(A, B)
+
+# Print the solution
+print("Solution to Linear Equations:")
+print(f"x = {x[0]}, y = {x[1]}")
