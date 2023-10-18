de7a24250b62774c1f02cabacb7596ac8f38cdbc
choudharymansi279 <choudharymansi279@gmail.com>
2023-10-18 22:37:49 +0530

    creating array numpy operation

---
+++ creating_array.np.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# create a 1D array
+arr_1d = np.array([1, 2, 3, 4, 5])
+
+# create a 2D array
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+
+# create a range of values
+arr_range = np.arange(0, 10, 2)
+
+# create a linearly spaced array
+arr_linspace = np.linspace(0, 1, 5)
+
+# create an array of zeros and ones
+arr_zeroes = np.zeros((2, 3))  
+arr_ones = np.ones((3, 2))
+
+print(arr_1d)
+print(arr_2d)
+print(arr_range)
+print(arr_linspace)
+print(arr_zeroes)
+print(arr_ones)
